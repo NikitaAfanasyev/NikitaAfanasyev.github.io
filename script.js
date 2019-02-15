c28d0a26975a01dad55caca576cf4da11a090898
Nikitos <kentuckyfriedtoad@gmail.com>
2019-02-15 16:26:17 +0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
 document.querySelector(".btn-authorize").addEventListener("click", authorize);
 
+
+console.log(document.cookie);
+
+var url_string = window.location.href;
+var url = new URL(url_string);
+var code = url.searchParams.get("code");
+if(typeof code == "string") {
+    document.cookie = "userName="+code;
+    console.log('cookie added')
+}
+
 function authorize() {
-    alert("huy");
+
 }
